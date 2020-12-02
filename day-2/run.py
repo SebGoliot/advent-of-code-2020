8a572b7d7e36380705e15f7e99c683079cb5fe6d
SebGoliot <50270166+SebGoliot@users.noreply.github.com>
2020-12-03 00:04:39 +0100

    Completed day 2 part 2

--- day-2/run.py
+++ day-2/run.py
@@ -9,6 +9,7 @@ def parse_input() -> list:
 
 input = parse_input()
 
+
 def get_valid_passwords(input):
     n = 0
     for pwd in input:
@@ -18,3 +19,18 @@ def get_valid_passwords(input):
     return n
 
 print(get_valid_passwords(input))
+
+
+def get_real_valid_passwords(input):
+    n = 0
+    for pwd in input:
+        i = 0
+        if pwd[3][int(pwd[0])-1] == pwd[2]:
+            i += 1
+        if pwd[3][int(pwd[1])-1] == pwd[2]:
+            i += 1
+        if i == 1:
+            n += 1
+    return n
+
+print(get_real_valid_passwords(input))
