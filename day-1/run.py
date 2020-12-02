ecc083cc03dbf0727979fdb128a2bf3a79994150
SebGoliot <50270166+SebGoliot@users.noreply.github.com>
2020-12-02 23:44:43 +0100

    Completed day 1 part 2

--- day-1/run.py
+++ day-1/run.py
@@ -15,3 +15,14 @@ def find_sum(input):
                 return
 
 find_sum(input)
+
+def find_sum_part_2(input):
+    for x in input:
+        for y in input:
+            for z in input:
+                if x + y + z == 2020:
+                    print(f"{x} + {y} + {z} = 2020 !")
+                    print(f"{x} x {y} x {z} = {x*y*z} !")
+                    return
+
+find_sum_part_2(input)
